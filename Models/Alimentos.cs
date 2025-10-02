665d5020257100065672b6c3503a7152f5eb84bf
Diogo Cardoso <1709197@sal.ipg.pt>
2025-10-02 15:29:32 +0100

    Created class Alimentos

---
+++ Models/Alimentos.cs
@@ -0,0 +1,9 @@
+﻿namespace HealthWellbeing.Models
+{
+    public class Alimentos
+    {
+        public int AlimentoId { get; set; }
+        public string Name { get; set; } = default!;
+        public string Description { get; set; }
+    }
+}
