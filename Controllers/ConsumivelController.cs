c3be3bc97e51154166da7ef4153c816802d048f0
Mendes444 <goncalo.m.s.mendes@gmail.com>
2025-10-02 14:22:31 +0100

    Criação de um form simples para inserir um novo consumivel e do respetivo controlador.

---
+++ Controllers/ConsumivelController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using HealthWellbeing.Models;
+
+namespace HealthWellbeing.Controllers
+{
+    public class ConsumivelController : Controller
+    {
+        public IActionResult ConsumivelRegister()
+        {
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
