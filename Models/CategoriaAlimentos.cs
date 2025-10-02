f12bc3445f6ac0a351c0330747dc92662c6a7e3c
Inesar16 <ines.arodrigues16@gmail.com>
2025-10-02 15:31:58 +0100

    created class categoria de alimentos

---
+++ Models/CategoriaAlimentos.cs
@@ -0,0 +1,11 @@
+﻿namespace HealthWellbeing.Models
+{
+    public class CategoriaAlimentos
+    {
+        public int CategoriaID { get; set; }
+
+        public string name { get; set; }
+
+        public string descricao { get; set; }
+    }
+}
