c741c794191e0dd88fec3475e7069615a7aad45c
beatrizsm00 <biamatias074@gmail.com>
2025-10-02 10:21:16 +0100

    create doctor model

---
+++ Models/Doctor.cs
@@ -0,0 +1,12 @@
+﻿namespace HealthWellbeing.Models
+{
+    public class Doctor {
+        public int DoctorId { get; set; }
+        public string DoctorName { get; set; }
+        public int DoctorPhone{get; set; }  
+        public string DoctorEmail { get; set; }  
+
+        public string DoctorSpeciality {  get; set; }
+
+        }
+}
