e6c1e0f2fc9f193ded7557936493736ed75528a5
Tiago Silva <1709436@sal.ipg.pt>
2025-10-02 15:47:50 +0100

    Created class ComponentesAlimentos

--- Models/ComponentesAlimentos.cs
+++ Models/ComponentesAlimentos.cs
@@ -5,7 +5,7 @@
         public int CompFoodID { get; set; } = default!;
 
         public string Name { get; set; } = default!;
-
+        //teste
         public int Description { get; set; } = default!;
     }
 }
