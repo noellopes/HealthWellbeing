96d41926ede9b7cde8b9f0cf9db6c92726bfb347
AfonsoFV <afonsofv@outlook.pt>
2025-10-02 10:09:07 +0100

    Insercao de Model de Registos

---
+++ Models/Register.cs
@@ -0,0 +1,13 @@
+﻿namespace HealthWellbeing.Models
+{
+    public class Register
+    {
+        public int registerId { get; set; }
+        public int pacientId { get; set; }
+        public int doctorId { get; set; }
+        public string date { get; set; }
+        public string diagnostic { get; set; }
+        public string observations { get; set; }
+
+    }
+}
