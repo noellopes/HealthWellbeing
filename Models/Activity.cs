675d2e7df481dadfd73d0637a3b560d92ce77bf9
RicardoMacedo-prj <1708940@sal.ipg.pt>
2025-10-02 16:01:31 +0100

    Create activity model

---
+++ Models/Activity.cs
@@ -0,0 +1,8 @@
+﻿namespace HealthWellbeing.Models {
+    public class Activity {
+
+        public int ActivityId { get; set; }
+        public string ActivityName { get; set; }
+        public string ActivityDescription { get; set; }
+    }
+}
