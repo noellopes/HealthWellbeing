7f70248d31dfb6dab93b0d3ffd8890f0fb4c0847
DFernandees <dofernandees@gmail.com>
2025-10-02 16:04:34 +0100

    Criação Model Membro

---
+++ Models/Membro.cs
@@ -0,0 +1,21 @@
+﻿namespace HealthWellbeing.Models
+{
+    public class Membro
+    {
+        public int MembroId { get; set; } = default!;
+        
+        // Sendo Membro já é Cliente, logo podemos ir buscar as informções como nome, etc a essa class
+
+        // Plano de cada membro (adicionar futuramente)
+        public bool? HaveAPlan { get; set; } = default!;
+        public string MembroType { get; set;} = default!;
+
+        public string HaveAPlanText => HaveAPlan switch
+        {
+            true => "Member plans are available",
+            false => "Member does not have a plan",
+            _ => "The member's plans are still being processed",
+        };
+
+    }
+}
