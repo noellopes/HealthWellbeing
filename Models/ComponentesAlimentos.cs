4a4cf49eb779e39d2fae19a7bb3732bb7ec5d05d
Tiago Silva <1709436@sal.ipg.pt>
2025-10-02 15:41:24 +0100

    Created Class Componentes Alimentos

---
+++ Models/ComponentesAlimentos.cs
@@ -0,0 +1,11 @@
+﻿namespace HealthWellbeing.Models
+{
+    public class ComponentesAlimentos
+    {
+        public int CompFoodID { get; set; } = default!;
+
+        public string Name { get; set; } = default!;
+
+        public int Description { get; set; } = default!;
+    }
+}
