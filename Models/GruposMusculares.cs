f26f4be4c6db2632a98eb59f51da903562871ba5
andrekhandoha10 <1709448@sal.ipg.pt>
2025-10-02 16:25:36 +0100

    Adição da Class Grupos Musculares

---
+++ Models/GruposMusculares.cs
@@ -0,0 +1,11 @@
+﻿namespace HealthWellbeing.Models
+{
+    public class GrupoMuscular
+    {
+        public int IdMusculo { get; set; }
+        public string Nome { get; set; }
+        public string RegiaoCorpo { get; set; }
+        public int TamanhoMusculo { get; set; }
+
+    }
+}
