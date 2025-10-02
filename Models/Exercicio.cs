d5b39dff79edac53e5fe073eb648b938a5e4a843
haper <hapereira2004@gmail.com>
2025-10-02 16:07:47 +0100

    Criação Modelo Exercicios

---
+++ Models/Exercicio.cs
@@ -0,0 +1,17 @@
+﻿namespace HealthWellbeing.Models
+{
+    public class Exercicio
+    {
+        public int ExercicioId { get; set; }
+
+        public string ExercicioName { get; set; } = string.Empty;
+
+        public string Descricao { get; set; } = string.Empty;
+
+        public double Duracao { get; set; } = 0;
+
+        public int Intencidade { get; set; } = 0;
+
+        public double CaloriasGastas { get; set; } = 0;
+    }
+}
