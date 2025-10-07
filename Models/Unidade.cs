bf4dc9a6be33ff8bec14c6b2ae1596cd3e11e40f
Emanuel0liveira <114081760+Emanuel0liveira@users.noreply.github.com>
2025-10-07 10:23:47 +0100

    Created class Unidade

---
+++ Models/Unidade.cs
@@ -0,0 +1,10 @@
+﻿namespace HealthWellbeing.Models
+{
+    public class Unidade
+    {
+        public int UnidadeId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+    }
+}
