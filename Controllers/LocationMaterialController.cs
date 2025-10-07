7301282037679f7bcb320f43c46a77d3fcda64a7
Marcel Moura <marcelmoura004@gmail.com>
2025-10-07 09:35:01 +0100

    foi criado a view e o controller da localização do material.

---
+++ Controllers/LocationMaterialController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HealthWellbeingRoom.Controllers
+{
+    public class LocationMaterialController : Controller
+    {
+        public IActionResult CreateLocationMaterial()
+        {
+            return View();
+        }
+
+        public IActionResult ReadLocationMateriall()
+        {
+            return View();
+        }
+
+        public IActionResult UpdateLocationMaterial()
+        {
+            return View();
+        }
+
+        public IActionResult DeleteLocationMaterial()
+        {
+            return View();
+        }
+    }
+}
