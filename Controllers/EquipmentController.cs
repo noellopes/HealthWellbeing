7116f161c7895effbefb40114af1a668054d9827
luzayadio <lbenivila@gmail.com>
2025-10-08 23:25:47 +0100

    Criação de MVC de Equipamento e add no menu

---
+++ Controllers/EquipmentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HealthWellbeingRoom.Controllers
+{
+    public class EquipmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
