cb473ea42f4095821234be724d68d369b034aa58
Tiago Silva <1709436@sal.ipg.pt>
2025-10-09 16:19:35 +0100

    Criação de Controlador ComponentesAlimentos

---
+++ Controllers/ComponentesAlimentoController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using HealthWellbeing.Models;
+
+namespace HealthWellbeing.Controllers
+{
+    public class ComponentesAlimentoController : Controller
+    {
+        // GET: ComponentesAlimentoController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: ComponentesAlimentoController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ComponentesAlimentoController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ComponentesAlimentoController/Create
+        [HttpPost]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(ComponentesAlimentos));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ComponentesAlimentoController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ComponentesAlimentoController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(ComponentesAlimentos));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ComponentesAlimentoController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ComponentesAlimentoController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(ComponentesAlimentos));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
