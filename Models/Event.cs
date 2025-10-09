ca8ec81df17c08dbbc797feea4e6f51e740132e4
RicardoMacedo-prj <1708940@sal.ipg.pt>
2025-10-09 15:47:36 +0100

    Create EventType class

--- Models/Event.cs
+++ Models/Event.cs
@@ -5,9 +5,8 @@
         public int EventId { get; set; }
         public string EventName { get; set; } = string.Empty;
         public string EventDescription { get; set; } = string.Empty;
-        public string EventType { get; set; }
-        public string EventTypeDescription { get; set; }
-        public string EventTypeName { get; set; }
+
+        public string EventPoints {  get; set; }
 
 
     }
---
+++ Models/EventType.cs
@@ -0,0 +1,9 @@
+﻿namespace HealthWellbeing.Models {
+    public class EventType {
+
+        public int EventTypeId { get; set; }
+        public string EventTypeName { get; set; }
+        public string EventTypeDescription { get; set; }
+
+    }
+}
