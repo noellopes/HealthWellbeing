da40ec9e1f829cab49ad4fff0e4fdeac532f1d61
Rafadex42 <rochamiguelbruno@gmail.com>
2025-10-09 09:59:45 +0100

    Atualizacao da View RegisterPathology e do Model Pathology

--- Models/Pathology.cs
+++ Models/Pathology.cs
@@ -8,6 +8,7 @@ namespace HealthWellbeing.Models
         public int PathologyId { get; set; }
 
         [Required(ErrorMessage = "Name is Required")]
+        [StringLength(200, MinimumLength = 3, ErrorMessage = "The name must have at least 3 chars and no more than 100 chars")]
         public string Name { get; set; }
 
         [Required(ErrorMessage = "Description is Required")]
