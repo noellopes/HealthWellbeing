5fb0c05bb9f0cf0ed6117ef4dcff9644f45841b8
leoIam12 <leonel.p.d.fernandes17@gmail.com>
2025-10-09 15:40:39 +0100

    Criação do MVC dos Dispositivos Móveis

---
+++ Controllers/MobileDevicesController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HealthWellbeingRoom.Controllers
+{
+    public class MobileDevicesController : Controller
+    {
+        public IActionResult CreateMobileDevices()
+        {
+            return View();
+        }
+
+        public IActionResult ReadMobileDevices()
+        {
+            return View();
+        }
+
+        public IActionResult UpdateMobileDevices()
+        {
+            return View();
+        }
+
+        public IActionResult DeleteMobileDevices()
+        {
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
