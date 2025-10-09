ba50e35902d0b6a8308656631132f08e5c7fcce6
Rafadex42 <rochamiguelbruno@gmail.com>
2025-10-09 09:38:57 +0100

    Insercao no HomeController da action para abrir a view do RegisterPatology,e criacao de um botao no index para utilizar essa action

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace HealthWellbeing.Controllers
             return View();
         }
 
+        public IActionResult RegisterPatology()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
