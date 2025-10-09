e6e78f5e8f3815cea9b5e8662c375b2c32db0651
dasilva-2024 <arturfilipe24@gmail.com>
2025-10-09 15:55:16 +0100

    Criação do medelo Terapeuta Criação do Modelo Terapeuta.

---
+++ Models/TerapeutaModel.cs
@@ -0,0 +1,16 @@
+﻿namespace HealthWellbeing.Models
+{
+    public class TerapeutaModel
+    {
+        public int TerapeutaId { get; set; }
+        public string Nome { get; set; }
+        public string Especialidade { get; set; }
+        public string Contacto { get; set; }
+        public string Email { get; set; }
+        public int AnosExperiencia { get; set; }
+        public bool Ativo { get; set; }
+
+
+    }
+}
+
