87f7506bf83f4f4a2f33dc7c039f451eb7aa24f1
andrekhandoha10 <1709448@sal.ipg.pt>
2025-10-09 15:17:55 +0100

    Adição do modelo GruposMusculares

---
+++ Models/GruposMusculares.cs
@@ -0,0 +1,15 @@
+﻿namespace HealthWellbeing.Models
+{
+    public class GruposMusculares
+    {
+        public int MusculoId { get; set; } // ID
+
+        public string MusculoNome { get; set; } // Nome do músculo
+
+        public string GrupoMuscularPrimario { get; set; } // Ex: Peitoral, Dorsal, Quadríceps...
+
+        public string LadoMusculo { get; set; } // Ex: Esquerdo ou Direito
+
+        public double TamanhoMusculo { get; set; } = 0.0; // Tamanho do músculo (em cm ou cm²)
+    }
+}
