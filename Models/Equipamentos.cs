20ee100345454e9621fd98610979be56f3cd67d7
massas24 <diogo.massano.ccmi@gmail.com>
2025-10-09 15:35:00 +0100

    Adicionei o modelo equipamentos

---
+++ Models/Equipamentos.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoSaude.Models
+{
+    public class Equipamento
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O nome do equipamento é obrigatório")]
+        [StringLength(100, ErrorMessage = "O nome não pode ter mais de 100 caracteres")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O tipo de equipamento é obrigatório")]
+        [StringLength(100, ErrorMessage = "O tipo não pode ter mais de 100 caracteres")]
+        [Display(Name = "Tipo de Equipamento")]
+        public string Tipo { get; set; }
+
+        [StringLength(200, ErrorMessage = "A descrição não pode ter mais de 200 caracteres")]
+        public string Descricao { get; set; }
+
+        [Display(Name = "Disponível?")]
+        public bool Disponivel { get; set; } = true; // por defeito está disponível
+
+        [Display(Name = "Data de Aquisição")]
+        [DataType(DataType.Date)]
+        public DateTime? DataAquisicao { get; set; }
+
+        [Display(Name = "Número de Série")]
+        [StringLength(50)]
+        public string NumeroSerie { get; set; }
+    }
+}
