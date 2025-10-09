45032b0976bd0ca76f9292e6154ccd0bc7b2ceb0
dasilva-2024 <arturfilipe24@gmail.com>
2025-10-09 16:25:05 +0100

    Criação do Modelo Agendamento

---
+++ Models/AgendamentoModel.cs
@@ -0,0 +1,19 @@
+﻿namespace HealthWellbeing.Models
+{
+    public class AgendamentoModel
+    {
+        public int AgendamentoId { get; set; }
+        public DateTime DataHoraInicio { get; set; }
+        public DateTime DataHoraFim { get; set; }
+        public string Estado { get; set; }// "Pendente", "Confirmado", "Cancelado"
+
+        public int UtenteBalnearioId { get; set; }
+        public UtenteBalneario UtenteBalneario { get; set; }
+        public int TerapeutaId {  get; set; }
+        public TerapeutaModel Terapeuta { get; set; }
+        public int ServicoId { get; set; }  
+        public ServicoModel Servico { get; set; } 
+
+
+    }
+}
