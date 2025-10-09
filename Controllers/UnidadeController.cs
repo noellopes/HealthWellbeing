cb67e8afa753bd7e4e5a216a4e7b5cc131db5f6e
Emanuel0liveira <114081760+Emanuel0liveira@users.noreply.github.com>
2025-10-09 16:09:21 +0100

    Criação do Controller e Inicio da View

---
+++ Controllers/UnidadeController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using HealthWellbeing.Models;
+
+namespace HealthWellbeing.Controllers
+{
+    public class UnidadeController : Controller
+    {
+        // GET: UnidadeController
+        public ActionResult Unidade()
+        {
+            return View();
+        }
+
+        // GET: UnidadeController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: UnidadeController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UnidadeController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Unidade));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UnidadeController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: UnidadeController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Unidade));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UnidadeController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: UnidadeController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Unidade));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
