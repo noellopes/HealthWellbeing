2d285cc3d3a4c73d303a9f958ddcf9eb44e7a87a
Emanuel0liveira <114081760+Emanuel0liveira@users.noreply.github.com>
2025-10-14 12:15:46 +0100

    Created Views, Controller and updated Model for Unidade/Porções

--- Controllers/UnidadeController.cs
+++ Controllers/UnidadeController.cs
@@ -1,84 +1,84 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
 using HealthWellbeing.Models;
 
 namespace HealthWellbeing.Controllers
 {
     public class UnidadeController : Controller
     {
-        // GET: UnidadeController
-        public ActionResult Unidade()
+        // “BD” temporária em memória
+        private static readonly List<Unidade> _data = new()
         {
-            return View();
+            new Unidade { UnidadeId = 1, NomeComida = "Pão",         Quantidade = "1 fatia (30 g)" },
+            new Unidade { UnidadeId = 2, NomeComida = "Leite",       Quantidade = "200 ml" },
+            new Unidade { UnidadeId = 3, NomeComida = "Arroz cozido",Quantidade = "250 g" }
+        };
+
+        // GET: /Unidade
+        public IActionResult Index() => View(_data.OrderBy(x => x.UnidadeId).ToList());
+
+        // GET: /Unidade/Details/5
+        public IActionResult Details(int id)
+        {
+            var item = _data.FirstOrDefault(x => x.UnidadeId == id);
+            if (item == null) return NotFound();
+            return View(item);
         }
 
-        // GET: UnidadeController/Details/5
-        public ActionResult Details(int id)
+        // GET: /Unidade/Create
+        public IActionResult Create() => View(new Unidade());
+
+        // POST: /Unidade/Create
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult Create(Unidade model)
         {
-            return View();
+            if (!ModelState.IsValid) return View(model);
+            model.UnidadeId = _data.Count == 0 ? 1 : _data.Max(x => x.UnidadeId) + 1;
+            _data.Add(model);
+            TempData["Msg"] = "Registo criado.";
+            return RedirectToAction(nameof(Index));
         }
 
-        // GET: UnidadeController/Create
-        public ActionResult Create()
+        // GET: /Unidade/Edit/5
+        public IActionResult Edit(int id)
         {
-            return View();
+            var item = _data.FirstOrDefault(x => x.UnidadeId == id);
+            if (item == null) return NotFound();
+            return View(item);
         }
 
-        // POST: UnidadeController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        // POST: /Unidade/Edit/5
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Unidade model)
         {
-            try
-            {
-                return RedirectToAction(nameof(Unidade));
-            }
-            catch
-            {
-                return View();
-            }
+            if (!ModelState.IsValid) return View(model);
+            var item = _data.FirstOrDefault(x => x.UnidadeId == id);
+            if (item == null) return NotFound();
+
+            item.NomeComida = model.NomeComida;
+            item.Quantidade = model.Quantidade;
+
+            TempData["Msg"] = "Registo atualizado.";
+            return RedirectToAction(nameof(Index));
         }
 
-        // GET: UnidadeController/Edit/5
-        public ActionResult Edit(int id)
+        // GET: /Unidade/Delete/5
+        public IActionResult Delete(int id)
         {
-            return View();
+            var item = _data.FirstOrDefault(x => x.UnidadeId == id);
+            if (item == null) return NotFound();
+            return View(item);
         }
 
-        // POST: UnidadeController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        // POST: /Unidade/Delete/5
+        [HttpPost, ActionName("Delete"), ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
         {
-            try
-            {
-                return RedirectToAction(nameof(Unidade));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: UnidadeController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: UnidadeController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Unidade));
-            }
-            catch
-            {
-                return View();
-            }
+            var item = _data.FirstOrDefault(x => x.UnidadeId == id);
+            if (item != null) _data.Remove(item);
+            TempData["Msg"] = "Registo removido.";
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- Models/Unidade.cs
+++ Models/Unidade.cs
@@ -1,10 +1,15 @@
-﻿namespace HealthWellbeing.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HealthWellbeing.Models
 {
     public class Unidade
     {
         public int UnidadeId { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
 
+        [Required, StringLength(60)]
+        public string NomeComida { get; set; } = string.Empty;
+
+        [Required, StringLength(30)]
+        public string Quantidade { get; set; } = string.Empty;
     }
 }
