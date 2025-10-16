fb52ea05c1bde204c8e92408b24d43705888975a
NunoClodic Carvalho Lima <nunoinformaticaipg@gmail.com>
2025-10-16 10:29:50 +0100

    Criado os botoes com as acoes Editar e Eliminar atualizado a disponibilidade para disponivel e indisponivel

--- Controllers/RoomController.cs
+++ Controllers/RoomController.cs
@@ -1,52 +1,90 @@
 ﻿using HealthWellbeing.Models;
 using HealthWellbeingRoom.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using NuGet.Protocol.Core.Types;
-using System.Diagnostics;
 
 namespace HealthWellbeingRoom.Controllers
 {
     public class RoomController : Controller
     {
+        // GET: Criar nova sala
         [HttpGet]
         public IActionResult CreateRoom()
         {
             return View();
         }
+
+        // POST: Criar nova sala
+        [HttpPost]
+        public IActionResult CreateRoom(Room room)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(room);
+            }
+
+            room.SalaId = RepositoryRoom.Rooms.Count() + 1;
+            RepositoryRoom.AddRoom(room);
+            return View("CreateRoomComplete", room);
+        }
+
+        // GET: Confirmação de criação
+        [HttpGet]
+        public IActionResult CreateRoomComplete()
+        {
+            return View();
+        }
+
+        // GET: Listar salas
         [HttpGet]
         public IActionResult ReadRoom()
         {
             var rooms = RepositoryRoom.Rooms;
             return View(rooms);
         }
+
+        // GET: Editar sala
         [HttpGet]
-        public IActionResult UpdateRoom()
+        public IActionResult UpdateRoom(int id)
         {
-            return View();
-        }
-        [HttpGet]
-        public IActionResult DeleteRoom()
-        {
-            return View();
+            var room = RepositoryRoom.GetRoomById(id);
+            if (room == null)
+            {
+                return NotFound();
+            }
+            return View(room);
         }
 
+        // POST: Atualizar sala
         [HttpPost]
-        public IActionResult CreateRoom(Room room)
+        public IActionResult UpdateRoom(Room room)
         {
-
             if (!ModelState.IsValid)
             {
-                return View(room); //se o modelo nao for valido, retorna a view com os erros
+                return View(room);
             }
-            room.SalaId = RepositoryRoom.Rooms.Count() + 1;
-            RepositoryRoom.AddRoom(room);
-            return View("CreateRoomComplete", room);
+
+            RepositoryRoom.UpdateRoom(room);
+            return RedirectToAction("ReadRoom");
         }
+
+        // GET: Eliminar sala
         [HttpGet]
-        public IActionResult CreateRoomComplete()
+        public IActionResult DeleteRoom(int id)
         {
-            return View();
+            var room = RepositoryRoom.GetRoomById(id);
+            if (room == null)
+            {
+                return NotFound();
+            }
+            return View(room);
+        }
+
+        // POST: Confirmar eliminação
+        [HttpPost, ActionName("DeleteRoom")]
+        public IActionResult ConfirmDeleteRoom(int id)
+        {
+            RepositoryRoom.RemoveRoom(id);
+            return RedirectToAction("ReadRoom");
         }
     }
 }
