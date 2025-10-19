16f7e857028d1ae961a4697197549f00edf36ed4
AfonsoFV <afonsofv@outlook.pt>
2025-10-19 16:20:24 +0100

    Create of the views for login and register a the controller for the accounts

--- Models/Register.cs
+++ Models/Register.cs
@@ -8,10 +8,15 @@ namespace HealthWellbeing.Models
 
         [Required(ErrorMessage = "Please enter the register id!")]
         public int registerId { get; set; } = default!;
+        [Required(ErrorMessage = "Please enter the pacient id!")]
         public int pacientId { get; set; } = default!;
+        [Required(ErrorMessage = "Please enter the doctor id!")]
         public int doctorId { get; set; } = default!;
+        [Required(ErrorMessage = "Please enter the date!")]
         public string date { get; set; } = default!;
+        [Required(ErrorMessage = "Please write the diagnostic!")]
         public string diagnostic { get; set; } = default!;
+        [Required(ErrorMessage = "Please write the observations!")]
         public string observations { get; set; } = default!;
 
     }
---
+++ Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HealthWellbeing.Models
+{
+    public class User
+    {
+        [Required(ErrorMessage = "Please enter your ID")]
+        public string UserId { get; set; } = default!;
+
+        [Required(ErrorMessage = "Please enter your password")]
+        [StringLength(20, MinimumLength = 4)]
+        public string Password { get; set; } = default!;
+
+        [Required]
+        public string Role { get; set; } = default!; 
+    }
+}
