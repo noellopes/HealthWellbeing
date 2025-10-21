eeb673176e34e5bb9fd4266b8f51946956b18459
Leo_Moraes <developerleo.it@gmail.com>
2025-10-21 20:03:39 -0300

    update RestricaoAlimentar

--- Controllers/RestricoesAlimentaresController.cs
+++ Controllers/RestricoesAlimentaresController.cs
@@ -2,23 +2,22 @@ using Microsoft.AspNetCore.Mvc;
 using HealthWellbeing.Models;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace HealthWellbeing.Controllers
 {
-    public class RestricoesAlimentaresController : Controller
+    public class RestricaoAlimentarController : Controller
     {
         // Simula um banco de dados em memória
-        private static List<RestricoesAlimentares> _restricoes = new List<RestricoesAlimentares>();
+        private static readonly List<RestricaoAlimentar> _restricoes = new List<RestricaoAlimentar>();
 
-        // GET: RestricoesAlimentares
-        public async Task<IActionResult> Index()
+        // GET: RestricaoAlimentar
+        public IActionResult Index()
         {
-            return await Task.FromResult(View(_restricoes));
+            return View(_restricoes);
         }
 
-        // GET: RestricoesAlimentares/Details/5
-        public async Task<IActionResult> Details(int? id)
+        // GET: RestricaoAlimentar/Details/5
+        public IActionResult Details(int? id)
         {
             if (id == null)
                 return NotFound();
@@ -27,31 +26,31 @@ namespace HealthWellbeing.Controllers
             if (restricao == null)
                 return NotFound();
 
-            return await Task.FromResult(View(restricao));
+            return View(restricao);
         }
 
-        // GET: RestricoesAlimentares/Create
+        // GET: RestricaoAlimentar/Create
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST: RestricoesAlimentares/Create
+        // POST: RestricaoAlimentar/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Nome,Tipo,Gravidade,Sintomas")] RestricoesAlimentares restricao)
+        public IActionResult Create([Bind("Id,Nome,Tipo,Gravidade,Sintomas")] RestricaoAlimentar restricao)
         {
             if (ModelState.IsValid)
             {
                 restricao.Id = _restricoes.Any() ? _restricoes.Max(r => r.Id) + 1 : 1;
                 _restricoes.Add(restricao);
-                return await Task.FromResult(RedirectToAction(nameof(Index)));
+                return RedirectToAction(nameof(Index));
             }
             return View(restricao);
         }
 
-        // GET: RestricoesAlimentares/Edit/5
-        public async Task<IActionResult> Edit(int? id)
+        // GET: RestricaoAlimentar/Edit/5
+        public IActionResult Edit(int? id)
         {
             if (id == null)
                 return NotFound();
@@ -60,13 +59,13 @@ namespace HealthWellbeing.Controllers
             if (restricao == null)
                 return NotFound();
 
-            return await Task.FromResult(View(restricao));
+            return View(restricao);
         }
 
-        // POST: RestricoesAlimentares/Edit/5
+        // POST: RestricaoAlimentar/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,Tipo,Gravidade,Sintomas")] RestricoesAlimentares restricao)
+        public IActionResult Edit(int id, [Bind("Id,Nome,Tipo,Gravidade,Sintomas")] RestricaoAlimentar restricao)
         {
             if (id != restricao.Id)
                 return NotFound();
@@ -82,13 +81,13 @@ namespace HealthWellbeing.Controllers
                 existente.Gravidade = restricao.Gravidade;
                 existente.Sintomas = restricao.Sintomas;
 
-                return await Task.FromResult(RedirectToAction(nameof(Index)));
+                return RedirectToAction(nameof(Index));
             }
             return View(restricao);
         }
 
-        // GET: RestricoesAlimentares/Delete/5
-        public async Task<IActionResult> Delete(int? id)
+        // GET: RestricaoAlimentar/Delete/5
+        public IActionResult Delete(int? id)
         {
             if (id == null)
                 return NotFound();
@@ -97,19 +96,19 @@ namespace HealthWellbeing.Controllers
             if (restricao == null)
                 return NotFound();
 
-            return await Task.FromResult(View(restricao));
+            return View(restricao);
         }
 
-        // POST: RestricoesAlimentares/Delete/5
+        // POST: RestricaoAlimentar/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
+        public IActionResult DeleteConfirmed(int id)
         {
             var restricao = _restricoes.FirstOrDefault(r => r.Id == id);
             if (restricao != null)
                 _restricoes.Remove(restricao);
 
-            return await Task.FromResult(RedirectToAction(nameof(Index)));
+            return RedirectToAction(nameof(Index));
         }
 
         private bool RestricaoExists(int id)
