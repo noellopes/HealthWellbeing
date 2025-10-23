b92781c68576659e3d190cc6f22a2861b3bf8c58
Tiago Silva <1709436@sal.ipg.pt>
2025-10-23 16:19:28 +0100

    Merged with Grupo-3 and translated Views, Controller and Model for FoodComponent

---
+++ Controllers/FoodComponentController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using HealthWellbeing.Models;
+
+namespace HealthWellbeing.Controllers
+{
+    public class FoodComponentController : Controller
+    {
+        private static List<FoodComponent> _CFood = new()
+        {
+            new FoodComponent { FoodComponentId  = 1, Name = "Apple", Description = "Water, Carbohydrates, Fibers, Vitamins, Minerals, Antioxidant, Maleic Acid"},
+            new FoodComponent { FoodComponentId = 2, Name = "Rice", Description = ""},
+        };
+
+
+        // GET: FoodComponentController
+        public IActionResult Index()
+        {
+            return View(_CFood);
+        }
+
+        // GET: FoodComponentController
+        public IActionResult Details(int id)
+        {
+            var a = _CFood.FirstOrDefault(x => x.FoodComponentId == id);
+            if (a == null) return NotFound();
+            return View(a);
+        }
+
+        // GET: FoodComponentController/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: FoodComponentController/Create
+        [HttpPost]
+        public IActionResult Create(FoodComponent a)
+        {
+            a.FoodComponentId = _CFood.Any() ? _CFood.Max(x => x.FoodComponentId) + 1 : 1;
+            _CFood.Add(a);
+            return RedirectToAction("Index");
+        }
+
+        // GET: FoodComponentController/Edit/5
+        public IActionResult Edit(int id)
+        {
+            var a = _CFood.FirstOrDefault(x => x.FoodComponentId == id);
+            if (a == null) return NotFound();
+            return View(a);
+        }
+
+        // POST: FoodComponentController/Edit/5
+        [HttpPost]
+        public IActionResult Edit(FoodComponent a)
+        {
+            var old = _CFood.FirstOrDefault(x => x.FoodComponentId == a.FoodComponentId);
+            if (old == null) return NotFound();
+            old.Name = a.Name;
+            old.Description = a.Description;
+            return RedirectToAction("Index");
+        }
+
+        // GET: FoodComponentController/Delete/5
+        public IActionResult Delete(int id)
+        {
+            var a = _CFood.FirstOrDefault(x => x.FoodComponentId == id);
+            if (a != null) _CFood.Remove(a);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/FoodComponent.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HealthWellbeing.Models
+{
+    public class FoodComponent
+    {
+        public int FoodComponentId { get; set; } = default!;
+
+        [Required(ErrorMessage = "The name of the Food is mandatory!")]
+        [StringLength(20)]
+        public string Name { get; set; } = default!;
+
+        [Required(ErrorMessage = "The Food has to have a mandatory description!")]
+        [StringLength(100)]
+        public string Description { get; set; } = default!;
+    }
+}
