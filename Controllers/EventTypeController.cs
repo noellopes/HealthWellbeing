bc4214de9554dd98f14091a80c75f007f5e63c03
RicardoMacedo-prj <1708940@sal.ipg.pt>
2025-10-23 16:02:23 +0100

    Creating event type view and controller configuration

--- Controllers/EventTypeController.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace HealthWellbeing.Controllers {
-    public class EventTypeController : Controller {
-        public IActionResult Index() {
-            return View();
-        }
-    }
-}
