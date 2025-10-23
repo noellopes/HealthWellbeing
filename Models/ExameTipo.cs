0346f6be9cf9cb4d9864d75acac5ef2298421d61
JoaquimGon <102263743+JoaquimGon@users.noreply.github.com>
2025-10-23 16:18:46 +0100

    Revert "Grupo 6"

--- Models/ExameTipo.cs
+++ Models/ExameTipo.cs
@@ -1,29 +1,19 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace HealthWellbeing.Models
 {
-    public class ExameTipo
+    public class ExameTipo : Controller
     {
-        public int ExameTipoId { get; set; }
+        public int Id { get; set; }
+        
+        [Required(ErrorMessage = "O nome do exame é obrigatório")]
+        [StringLength(100)]
+        public string Nome { get; set; }
 
-        [Required(ErrorMessage = "O nome do exame é obrigatório.")]
-        [StringLength(100, ErrorMessage = "O nome não pode ter mais de 100 caracteres.")]
-        public string Nome { get; set; } 
-
-        [StringLength(500, ErrorMessage = "A descrição não pode ter mais de 500 caracteres.")]
+        [StringLength(500)]
         public string Descricao { get; set; }
 
-<<<<<<< HEAD
-        [Required(ErrorMessage = "A especialidade é obrigatória.")]
-        [StringLength(100)]
-        public string Especialidade { get; set; }
-        
-
-        
-
-        
-=======
->>>>>>> master
     }
 
   
