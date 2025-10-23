e3b7748fea31a8c014b748eda57e4b3ad88f6569
marcelmm04 <marcelmoura004@gmail.com>
2025-10-23 09:49:35 +0100

    foi adicionado validações no modelo

--- Models/LocationMaterial.cs
+++ Models/LocationMaterial.cs
@@ -15,11 +15,16 @@ namespace HealthWellbeing.Models
         [StringLength(100, ErrorMessage = "O nome da sala não pode exceder 100 caracteres.")]
         public string Armario { get; set; }
         [Required(ErrorMessage = "O nome do armário é obrigatório.")]
+        [StringLength(100, ErrorMessage = "O nome do armário não pode exceder 100 caracteres.")]
         public string Gaveta { get; set; }
         [Required(ErrorMessage = "O número da gaveta é obrigatório.")]
+        [StringLength(50, ErrorMessage = "A gaveta não pode exceder 50 caracteres.")]
         public string Prateleira { get; set; }
+        [Required(ErrorMessage = "O número ou nome da prateleira é obrigatório.")]
+        [StringLength(50, ErrorMessage = "A prateleira não pode exceder 50 caracteres.")]
         public string CodigoIdentificacao { get; set; }
         [Required(ErrorMessage = "O código de identificação é obrigatório.")]
+        [StringLength(50, ErrorMessage = "O código de identificação não pode exceder 50 caracteres.")]
         public string Observacao { get; set; }
         
 
