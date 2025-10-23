a98127a90821a336644438ac8d3adcb4107974a6
luzayadio <lbenivila@gmail.com>
2025-10-23 09:26:24 +0100

    merge com master, pegando alterações de config BD

---
+++ Models/Patology.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HealthWellbeing.Models
+{
+    public class Patology
+    {
+        [Required(ErrorMessage = "ID is Required")]
+        public int PatologyId { get; set; }
+
+        [Required(ErrorMessage = "Name is Required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Description is Required")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Severity is Required")]
+        public string Severity { get; set; }
+
+    }
+}
