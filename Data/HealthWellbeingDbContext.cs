579d8d1f26efb3620d85b22e5f9804cf17e16dc4
Lucas <lcluz@sga.pucminas.br>
2025-10-23 11:17:19 +0100

    Remove Receita model and controller; add Receita DbSet to HealthWellbeingDbContext

--- Data/HealthWellbeingDbContext.cs
+++ Data/HealthWellbeingDbContext.cs
@@ -15,5 +15,6 @@ namespace HealthWellbeing.Data
         }
         public DbSet<HealthWellbeing.Models.Alergia> Alergia { get; set; } = default!;
         public DbSet<HealthWellbeing.Models.RestricaoAlimentar> RestricaoAlimentar { get; set; } = default!;
+        public DbSet<HealthWellbeing.Models.Receita> Receita { get; set; } = default!;
     }
 }
