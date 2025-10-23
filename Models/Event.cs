a85cfa20a3abd2b1ea5c6cf0d39dbb3244cd1a90
Diogo Aguiar <diogao2004@gmail.com>
2025-10-23 15:13:13 +0100

    -Created Event , Added EventsController, Created Views/Events folder

--- Models/Event.cs
+++ Models/Event.cs
@@ -5,9 +5,10 @@
         public int EventId { get; set; }
         public string EventName { get; set; } = string.Empty;
         public string EventDescription { get; set; } = string.Empty;
-
-        public string EventPoints {  get; set; }
-
-
+        public string EventType { get; set; } = string.Empty;
+        public int DurationMinutes { get; set; }
+        public string Intensity { get; set; } = string.Empty;
+        public int CaloriesBurned { get; set; }
+        public DateTime EventDate { get; set; } = DateTime.Now;
     }
 }
