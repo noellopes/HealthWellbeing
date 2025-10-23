46d25e3130aeb7bccaf8d96543f3b2080ad17891
Noel Lopes <noel@ipg.pt>
2025-10-23 10:14:02 +0100

    Fix ApplicationDbContext

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -10,19 +10,5 @@ namespace HealthWellbeing.Data
             : base(options)
         {
         }
-
-        //(CRUD de alimentos)
-        public DbSet<Alimento> Alimentos { get; set; } = default!;
-        public DbSet<CategoriaAlimento> Categorias { get; set; } = default!;
-
-        
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            base.OnModelCreating(builder);
-
-            //Nomes de tabelas personalizados
-            builder.Entity<Alimento>().ToTable("Alimento");
-            builder.Entity<CategoriaAlimento>().ToTable("Categoria");
-        }
     }
 }
