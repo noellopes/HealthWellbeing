2633fbfb4edbb54f3e5ba0b57236f685cbe6fabc
SuriGOAT <dinisgomes18@gmail.com>
2025-10-23 16:19:05 +0100

    Update do Modelo PE

--- Models/ProfissionalExecutante.cs
+++ Models/ProfissionalExecutante.cs
@@ -7,10 +7,23 @@ namespace HealthWellbeing.Models
         [Key]
         public int ProfissionalExecutanteId { get; set; } // Chave primária
 
+        [Required(ErrorMessage = "O nome é obrigatório")]
+        [StringLength(100)]
         public string Nome { get; set; }
+
+
+        [Required(ErrorMessage = "A função é obrigatória")]
+        [StringLength(150)]
         public string Funcao { get; set; } // Futuramente poderá ser uma entidade (e.g. tabela Função)
 
+
+
+        [Required(ErrorMessage = "O numero é obrigatório")]
+        [StringLength(15)]
         public string Telefone { get; set; }
+
+        [Required(ErrorMessage = "O email é obrigatório")]
+        [StringLength(250)]
         public string Email { get; set; }
 
     }
