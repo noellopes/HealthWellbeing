f187c9b6d3d04ab17e64a8c1fedb8b78f522cfc7
Rafaelg2t <155028289+Rafaelg2t@users.noreply.github.com>
2025-10-23 21:16:54 +0100

    Correção do modelo 'Exame'

--- Models/Exame.cs
+++ Models/Exame.cs
@@ -1,25 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace HealthWellbeing.Models
 {
     public class Exame
     {
-        public int examesId { get; set; }
+        [Key]
+        public int ExameId { get; set; }
 
-        [StringLength(100)]
-        public string marcacaodeutente { get; set; }
+        [Required(ErrorMessage = "O nome do utente é obrigatório.")]
+        [StringLength(100, ErrorMessage = "O nome do utente não pode ter mais de 100 caracteres.")]
+        [Display(Name = "Nome do Utente")]
+        public string NomeUtente { get; set; }
 
-        public DateTime data { get; set; }
+        [Required(ErrorMessage = "A data do exame é obrigatória.")]
+        [DataType(DataType.Date)]
+        [Display(Name = "Data do Exame")]
+        public DateTime Data { get; set; }
 
-        public ExameTipo exameTipo { get; set; }
+        [Required(ErrorMessage = "O tipo de exame é obrigatório.")]
+        [StringLength(50)]
+        [Display(Name = "Tipo de Exame")]
+        public string TipoExame { get; set; }
 
-        //public Medicos medicos { get; set; }
-
-
-
-
-
-
+        [StringLength(200)]
+        [Display(Name = "Observações")]
+        public string Observacoes { get; set; }
     }
 }
