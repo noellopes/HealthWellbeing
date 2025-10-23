a3707f7eee83c2334a2e1c3973e341820228bc79
Evanilson11 <1708452@sal.ipg.pt>
2025-10-23 10:36:22 +0100

    add scaffolding

--- Models/TypeMaterial.cs
+++ Models/TypeMaterial.cs
@@ -4,22 +4,22 @@ namespace HealthWellbeing.Models
 {
     public class TypeMaterial
     {
-        public int TipoMaterialID {  get; set; }
+        public int TypeMaterialID {  get; set; }
         [Required(ErrorMessage = "O nome do TipoMaterial é obrigatorio.")]
         [StringLength(100, ErrorMessage = "O nome não pode exceder 100 caracteres")]
-        public string TipoMaterialNome { get; set; }
+        public string Name { get; set; }
 
-        public string Descricao { get; set; }
+        public string Description { get; set; }
         [StringLength(255, ErrorMessage = "A descrição não pode exceder 255 caracteres.")]
 
-        public string TipoMaterialCategoria { get; set; }
+        public string Category { get; set; }
         [StringLength(50, ErrorMessage = "A categoria não pode exceder 50 caracteres.")]
        
 
-        public bool Ativo { get; set; } = true;
+        public bool Active { get; set; } = true;
 
        
-        public DateTime DataCriacao { get; set; } = DateTime.Now;
+        public DateTime DataCreate { get; set; } = DateTime.Now;
         [Required(ErrorMessage = "A data de criação é obrigatória.")]
 
         public DateTime? DataAtualizacao { get; set; }
