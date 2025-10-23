7792a27d7fcc0f71aa97c428b364b4e6e93694f3
Pedro Barreiros <pedrombarreiros12@gmail.com>
2025-10-23 15:19:49 +0100

    Added more elements

--- Models/Levels.cs
+++ Models/Levels.cs
@@ -2,8 +2,10 @@
 {
     public class Levels
     {
-        public int Level { get; set; }
-        public string Description { get; set; }=default!;   
+        public int LevelId { get; set; } // Primary Key
+        public int Level { get; set; } // Level number 1-100
+        public string LevelCategory { get; set; } // Level Category (<=25 beginner, <=50 intermediate,<=100 master)
+        public string Description { get; set; }=default!;  // Description 
 
     }
 }
