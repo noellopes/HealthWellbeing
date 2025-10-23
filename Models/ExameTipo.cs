c6a501bd9195dea84db28b1263ddd51d4e82d87b
massas24 <diogo.massano.ccmi@gmail.com>
2025-10-23 09:01:11 +0100

    Ãdicionei Especialidade e corrigi mais algumas coisas

--- Models/ExameTipo.cs
+++ Models/ExameTipo.cs
@@ -1,21 +1,23 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace HealthWellBeing.Models
+namespace HealthWellbeing.Models
 {
     public class ExameTipo
     {
-       
         public int ExameTipoId { get; set; }
 
-        
         [Required(ErrorMessage = "O nome do exame é obrigatório.")]
         [StringLength(100, ErrorMessage = "O nome não pode ter mais de 100 caracteres.")]
-        public string Nome { get; set; }
+        public string Nome { get; set; } // Ex: "Ressonância Magnética", "Análise de Sangue"
 
-        
         [StringLength(500, ErrorMessage = "A descrição não pode ter mais de 500 caracteres.")]
         public string Descricao { get; set; }
 
+        // Especialidade ou categoria (Imagiologia, Cardiologia, etc.)
+        [Required(ErrorMessage = "A especialidade é obrigatória.")]
+        [StringLength(100)]
+        public string Especialidade { get; set; }
 
+        
     }
 }
