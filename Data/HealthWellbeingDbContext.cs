42b9d0458af6f96ca729eb5225accfe81e1035a7
Noel Lopes <noel@ipg.pt>
2025-10-23 07:06:36 +0100

    Add HealthWellbeingDbContext

---
+++ Data/HealthWellbeingDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using HealthWellbeing.Models;
+
+namespace HealthWellbeing.Data
+{
+    public class HealthWellbeingDbContext : DbContext
+    {
+        public HealthWellbeingDbContext (DbContextOptions<HealthWellbeingDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
