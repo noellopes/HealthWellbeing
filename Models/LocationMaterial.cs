5668dfc68113ba8eb4c5a44b4ce331c3c6e1c74f
marcelmm04 <marcelmoura004@gmail.com>
2025-10-23 10:01:38 +0100

    o código foi mudado para inglês

--- Models/LocationMaterial.cs
+++ Models/LocationMaterial.cs
@@ -6,27 +6,28 @@ namespace HealthWellbeing.Models
     {
 
         public int LocationMaterialID { get; set; }
-        
-        public string Setor { get; set; }
-        [Required(ErrorMessage = "O tipo de setor é obrigatório.")]
-        [StringLength(100, ErrorMessage = "O setor não pode exceder 100 caracteres.")]
-        public string Sala { get; set; }
-        [Required(ErrorMessage = "O mome da sala é obrigatório.")]
-        [StringLength(100, ErrorMessage = "O nome da sala não pode exceder 100 caracteres.")]
-        public string Armario { get; set; }
-        [Required(ErrorMessage = "O nome do armário é obrigatório.")]
-        [StringLength(100, ErrorMessage = "O nome do armário não pode exceder 100 caracteres.")]
-        public string Gaveta { get; set; }
-        [Required(ErrorMessage = "O número da gaveta é obrigatório.")]
-        [StringLength(50, ErrorMessage = "A gaveta não pode exceder 50 caracteres.")]
-        public string Prateleira { get; set; }
-        [Required(ErrorMessage = "O número ou nome da prateleira é obrigatório.")]
-        [StringLength(50, ErrorMessage = "A prateleira não pode exceder 50 caracteres.")]
-        public string CodigoIdentificacao { get; set; }
-        [Required(ErrorMessage = "O código de identificação é obrigatório.")]
-        [StringLength(50, ErrorMessage = "O código de identificação não pode exceder 50 caracteres.")]
-        public string Observacao { get; set; }
-        
+
+        public string Sector { get; set; }
+        [Required(ErrorMessage = "The sector type is required.")]
+        [StringLength(100, ErrorMessage = "The sector cannot exceed 100 characters.")]
+        public string Room { get; set; }
+        [Required(ErrorMessage = "The room name is required.")]
+        [StringLength(100, ErrorMessage = "The room name cannot exceed 100 characters.")]
+        public string Cabinet { get; set; }
+        [Required(ErrorMessage = "The cabinet name is required.")]
+        [StringLength(100, ErrorMessage = "The cabinet name cannot exceed 100 characters.")]
+        public string Drawer { get; set; }
+        [Required(ErrorMessage = "The drawer number is required.")]
+        [StringLength(50, ErrorMessage = "The drawer cannot exceed 50 characters.")]
+        public string Shelf { get; set; }
+        [Required(ErrorMessage = "The shelf number or name is required.")]
+        [StringLength(50, ErrorMessage = "The shelf cannot exceed 50 characters.")]
+        public string IdentificationCode { get; set; }
+        [Required(ErrorMessage = "The identification code is required.")]
+        [StringLength(50, ErrorMessage = "The identification code cannot exceed 50 characters.")]
+        public string Observation { get; set; }
+
+
 
 
     }
