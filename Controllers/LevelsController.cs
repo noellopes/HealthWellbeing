e70b169e26dc800f32cf22e4a8797059278a88c9
Pedro Barreiros <pedrombarreiros12@gmail.com>
2025-10-23 15:55:51 +0100

    Created controller and view. Added colors to levels

---
+++ Controllers/LevelsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using HealthWellbeing.Models;
+
+namespace HealthWellbeing.Controllers
+{
+    public class LevelsController : Controller
+    {
+        public IActionResult Index(int level = 1)
+        {
+            if (level < 1) level = 1;
+            if (level > 100) level = 100;
+
+            var model = new Levels(level);
+            return View(model);
+        }
+    }
+}
--- Models/Levels.cs
+++ Models/Levels.cs
@@ -4,8 +4,35 @@
     {
         public int LevelId { get; set; } // Primary Key
         public int Level { get; set; } // Level number 1-100
-        public string LevelCategory { get; set; } // Level Category (<=25 beginner, <=50 intermediate,<=100 master)
+        public string LevelCategory { get; set; } // Level Category (If X level then Y category and Z circle color)
         public string Description { get; set; }=default!;  // Description 
+
+
+        public Levels(int level)
+        {
+            Level = level;
+            LevelCategory = GetCircleColor(level);
+        }
+
+        private string GetCircleColor(int level)
+        {
+            if (level < 25)
+            {
+                LevelCategory = "Beginner";
+                return "green";
+            }
+            else if (level < 50)
+            {
+                LevelCategory = "Intermediate";
+                return "blue";
+            }
+            else if (level < 75)
+            {
+                LevelCategory = "Intermediate";
+                return "yellow";
+            }
+            return "red";
+        }
 
     }
 }
