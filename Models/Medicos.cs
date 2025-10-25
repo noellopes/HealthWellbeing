6ce28c84649677c2a121516bfffaa6cf2b90b90e
AfonsoFV <afonsofv@outlook.pt>
2025-10-25 22:26:50 +0100

    Reapply "Merge branch 'Group-4' into Registo/HistóricoConsultas"
    This reverts commit 74b141853412d80bc932b1434f559564290d8531.

---
+++ Models/Medicos.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace HealthWellbeing.Models
+{
+	public class Medicos
+	{
+		public int Id { get; set; }
+
+		[Required(ErrorMessage = "O nome do médico é obrigatório")]
+		[StringLength(100)]
+		public string Nome { get; set; }
+
+		public string Especialidade {  get; set; }
+		public string Telefone { get; set; }
+		public string Email { get; set; }
+
+
+	}
+}
