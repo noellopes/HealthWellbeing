5d20bc6c9f2b49e813809f77a3134c8ee46e9250
MartimRed <martimfv@outlook.pt>
2025-10-26 20:09:25 +0000

    Recriação do SpecialitiesController, criação do CRUD das Specialities e modificação do model

--- Controllers/SpecialitiesController.cs
+++ Controllers/SpecialitiesController.cs
@@ -1,36 +1,157 @@
-﻿using HealthWellbeing.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using HealthWellbeing.Data;
+using HealthWellbeing.Models;
 
 namespace HealthWellbeing.Controllers
 {
     public class SpecialitiesController : Controller
     {
-        // GET: /Specialities
-        public IActionResult Index()
-        {
-            // Dados MOCK só para testar a página
-            var especialidades = new List<Specialities>
-            {
-                new() { IdEspecialidade = 1, Nome = "Cardiologia", Descricao = "Coração e sistema cardiovascular." },
-                new() { IdEspecialidade = 2, Nome = "Dermatologia", Descricao = "Pele, cabelo e unhas." },
-                new() { IdEspecialidade = 3, Nome = "Pediatria",   Descricao = "Saúde de crianças e adolescentes." }
-            };
+        private readonly HealthWellbeingDbContext _context;
 
-            // Envia a lista para a View (Views/Specialities/Index.cshtml)
-            return View(especialidades);
+        public SpecialitiesController(HealthWellbeingDbContext context)
+        {
+            _context = context;
         }
 
-        // GET: /Specialities/Details/5  (opcional, para página de detalhe)
-        public IActionResult Details(int id)
+        // GET: Specialities
+        public async Task<IActionResult> Index()
         {
-            var esp = new Specialities
-            {
-                IdEspecialidade = id,
-                Nome = "Exemplo",
-                Descricao = "Descrição da especialidade…"
-            };
+            return View(await _context.Specialities.ToListAsync());
+        }
 
-            return View(esp); // View: Views/Specialities/Details.cshtml
+        // GET: Specialities/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var specialities = await _context.Specialities
+                .FirstOrDefaultAsync(m => m.IdEspecialidade == id);
+            if (specialities == null)
+            {
+                return NotFound();
+            }
+
+            return View(specialities);
+        }
+
+        // GET: Specialities/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Specialities/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdEspecialidade,Nome,Descricao")] Specialities specialities)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(specialities);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(specialities);
+        }
+
+        // GET: Specialities/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var specialities = await _context.Specialities.FindAsync(id);
+            if (specialities == null)
+            {
+                return NotFound();
+            }
+            return View(specialities);
+        }
+
+        // POST: Specialities/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdEspecialidade,Nome,Descricao")] Specialities specialities)
+        {
+            if (id != specialities.IdEspecialidade)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(specialities);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SpecialitiesExists(specialities.IdEspecialidade))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(specialities);
+        }
+
+        // GET: Specialities/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var specialities = await _context.Specialities
+                .FirstOrDefaultAsync(m => m.IdEspecialidade == id);
+            if (specialities == null)
+            {
+                return NotFound();
+            }
+
+            return View(specialities);
+        }
+
+        // POST: Specialities/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var specialities = await _context.Specialities.FindAsync(id);
+            if (specialities != null)
+            {
+                _context.Specialities.Remove(specialities);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SpecialitiesExists(int id)
+        {
+            return _context.Specialities.Any(e => e.IdEspecialidade == id);
         }
     }
 }
