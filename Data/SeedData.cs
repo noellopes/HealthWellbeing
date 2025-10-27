3a8271836632256d98b771509553641dc14fa510
HenriqueADiniz <henriquealdiniz@gmail.com>
2025-10-27 11:55:29 +0000

    Seeder da Alergia

---
+++ Data/SeedData.cs
@@ -0,0 +1,71 @@
+﻿using HealthWellbeing.Data;
+using HealthWellbeing.Models;
+
+internal class SeedData
+{
+    public static void Populate(HealthWellbeingDbContext? context)
+    {
+        if (context == null)
+        {
+            throw new ArgumentNullException(nameof(context));
+        }
+
+        context.Database.EnsureCreated();
+
+        PopulateAlergias(context);
+    }
+
+    private static void PopulateAlergias(HealthWellbeingDbContext context)
+    {
+        // Só adiciona dados se ainda não existir nenhuma alergia cadastrada
+        if (context.Alergia.Any())
+        {
+            return;
+        }
+
+        context.Alergia.AddRange(
+            new Alergia
+            {
+                Nome = "Alergia ao Amendoim",
+                Descricao = "Reação alérgica grave a proteínas do amendoim.",
+                Gravidade = "Alta",
+                Sintomas = "Urticária, dificuldade para respirar, inchaço na garganta.",
+                AlimentoId = 2 
+            },
+            new Alergia
+            {
+                Nome = "Alergia ao Leite",
+                Descricao = "Sensibilidade às proteínas do leite de vaca.",
+                Gravidade = "Média",
+                Sintomas = "Cólicas, diarreia, erupções cutâneas.",
+                AlimentoId = 2
+            },
+            new Alergia
+            {
+                Nome = "Alergia ao Ovo",
+                Descricao = "Reação imunológica às proteínas da clara ou gema do ovo.",
+                Gravidade = "Baixa",
+                Sintomas = "Coceira, vermelhidão, desconforto gastrointestinal.",
+                AlimentoId = 2
+            },
+            new Alergia
+            {
+                Nome = "Alergia ao Trigo",
+                Descricao = "Reação às proteínas do trigo, incluindo o glúten.",
+                Gravidade = "Média",
+                Sintomas = "Inchaço abdominal, erupções cutâneas, fadiga.",
+                AlimentoId = 2
+            },
+            new Alergia
+            {
+                Nome = "Alergia a Frutos do Mar",
+                Descricao = "Reação a crustáceos e moluscos.",
+                Gravidade = "Alta",
+                Sintomas = "Inchaço facial, vômitos, anafilaxia.",
+                AlimentoId = 2
+            }
+        );
+
+        context.SaveChanges();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -20,15 +20,20 @@ builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseMigrationsEndPoint();
-}
-else
+if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+else
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var context = scope.ServiceProvider.GetRequiredService<HealthWellbeingDbContext>();
+        SeedData.Populate(context);
+    }
+
 }
 
 using (var scope = app.Services.CreateScope())
