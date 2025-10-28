b250abefe379e2ae1b309d28b426cab690f43a4d
Mendes444 <goncalo.m.s.mendes@gmail.com>
2025-10-28 10:52:45 +0000

    Criação do Model e do Controller para Zona de armazenamento já com o CRUD incluido

---
+++ Controllers/ZonasArmazenamentoController.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using HealthWellbeing.Data;
+using HealthWellbeing.Models;
+
+namespace HealthWellbeing.Controllers
+{
+    public class ZonasArmazenamentoController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ZonasArmazenamentoController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ZonasArmazenamento
+        public IActionResult Index()
+        {
+            var zonas = _context.ZonasArmazenamento.ToList();
+            return View(zonas);
+        }
+
+        // GET: ZonasArmazenamento/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ZonasArmazenamento/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(ZonaArmazenamento zona)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.ZonasArmazenamento.Add(zona);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(zona);
+        }
+
+        // GET: ZonasArmazenamento/Edit/5
+        public IActionResult Edit(int id)
+        {
+            var zona = _context.ZonasArmazenamento.Find(id);
+            if (zona == null)
+            {
+                return NotFound();
+            }
+            return View(zona);
+        }
+
+        // POST: ZonasArmazenamento/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, ZonaArmazenamento zona)
+        {
+            if (id != zona.Id)
+                return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                _context.ZonasArmazenamento.Update(zona);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(zona);
+        }
+
+        // GET: ZonasArmazenamento/Delete/5
+        public IActionResult Delete(int id)
+        {
+            var zona = _context.ZonasArmazenamento.Find(id);
+            if (zona == null)
+            {
+                return NotFound();
+            }
+            return View(zona);
+        }
+
+        // POST: ZonasArmazenamento/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var zona = _context.ZonasArmazenamento.Find(id);
+            if (zona != null)
+            {
+                _context.ZonasArmazenamento.Remove(zona);
+                _context.SaveChanges();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: ZonasArmazenamento/Details/5
+        public IActionResult Details(int id)
+        {
+            var zona = _context.ZonasArmazenamento.Find(id);
+            if (zona == null)
+            {
+                return NotFound();
+            }
+            return View(zona);
+        }
+    }
+}
---
+++ Models/ZonaArmazenamento.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HealthWellbeing.Models
+{
+    public class ZonaArmazenamento
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Nome { get; set; }
+
+        [StringLength(200)]
+        public string? Descricao { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        public string Localizacao { get; set; }
+
+        [Range(0, 10000)]
+        public double CapacidadeMaxima { get; set; }
+
+        public bool Ativa { get; set; } = true;
+    }
+}
