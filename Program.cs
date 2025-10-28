a3bd2988deb31623a92dba50746b8124fe130591
AfonsoFV <afonsofv@outlook.pt>
2025-10-28 01:51:34 +0000

    Popular com dados

--- Program.cs
+++ Program.cs
@@ -33,9 +33,11 @@ else
 
 using (var scope = app.Services.CreateScope())
 {
-    var services = scope.ServiceProvider;
-    var context = services.GetRequiredService<ApplicationDbContext>();
-    context.Database.Migrate();
+    var dbContext = scope.ServiceProvider.GetService<HealthWellbeingDbContext>();
+    //var services = scope.ServiceProvider;
+    //var context = services.GetRequiredService<HealthWellbeingDbContext>();
+    //context.Database.Migrate();
+    SeedData.Populate(dbContext);
 }
 
 app.UseHttpsRedirection();
