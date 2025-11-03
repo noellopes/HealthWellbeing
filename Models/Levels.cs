161ba0c7901e5e0d0f4159ab67b23c2e70bbd212
Tubias <pedrombarreiros12@gmail.com>
2025-11-03 20:54:06 +0000

    Deleted a DbContext and added LevelPoints to the model

--- Models/Levels.cs
+++ Models/Levels.cs
@@ -9,6 +9,8 @@ namespace HealthWellbeing.Models
         [Range(1, 100, ErrorMessage = "Level must be 1-100")]
         public int Level { get; set; } // Level number 1-100
         [ValidateNever]
+        public string LevelPoints { get; set; } // Total points of the level
+        [ValidateNever]
         public string LevelCategory { get; set; } // Level Category (If X level then Y category and Z circle color)
         [ValidateNever]
         public string Description { get; set; }  // Description 
