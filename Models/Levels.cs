15dd61f43e5a0018124fa43f64f5a6c07d75a808
Tubias <pedrombarreiros12@gmail.com>
2025-11-04 19:29:15 +0000

    Bug Fix

--- Models/Levels.cs
+++ Models/Levels.cs
@@ -8,8 +8,8 @@ namespace HealthWellbeing.Models
         public int LevelId { get; set; } // Primary Key
         [Range(1, 100, ErrorMessage = "Level must be 1-100")]
         public int Level { get; set; } // Level number 1-100
-        [ValidateNever]
-        public string LevelPoints { get; set; } // Total points of the level
+        //[ValidateNever]
+        //public string LevelPoints { get; set; } // Total points of the level
         [ValidateNever]
         public string LevelCategory { get; set; } // Level Category (If X level then Y category and Z circle color)
         [ValidateNever]
