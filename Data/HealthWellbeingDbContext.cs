b07a066b6c0f40aef46e5e5f4e0ad8421955bb6e
DiogoGH <diogorsilva04@gmail.com>
2025-11-05 14:25:50 +0000

    database create activity

--- Data/HealthWellbeingDbContext.cs
+++ Data/HealthWellbeingDbContext.cs
@@ -16,5 +16,6 @@ namespace HealthWellbeing.Data
         public DbSet<HealthWellbeing.Models.EventType> EventType { get; set; } = default!;
         public DbSet<HealthWellbeing.Models.Levels> Levels { get; set; } = default!;
         public DbSet<HealthWellbeing.Models.Event> Events { get; set; } = default!;
+        public DbSet<HealthWellbeing.Models.Activity_> Activity { get; set; } = default!;
     }
 }
