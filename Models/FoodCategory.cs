f1fd64ebbf4d2bad29420dc33e55af861b9ac98e
Inesar16 <ines.arodrigues16@gmail.com>
2025-11-05 15:13:02 +0000

    Update model view and controller of food category

--- Models/FoodCategory.cs
+++ Models/FoodCategory.cs
@@ -6,9 +6,9 @@ namespace HealthWellbeing.Models
 {
     public class FoodCategory
     {
-        [Key]
         public int FoodCategoryId { get; set; }
 
+<<<<<<< Updated upstream
         [Required(ErrorMessage = "Category name is required.")]
         [StringLength(100, MinimumLength = 2,
             ErrorMessage = "Category name must be between 2 and 100 characters.")]
@@ -70,5 +70,19 @@ namespace HealthWellbeing.Models
                     new[] { nameof(Description) });
             }
         }
+=======
+        [Required, StringLength(60)]
+        public string Name { get; set; } = string.Empty;
+
+        [StringLength(200)]
+        public string? Description { get; set; }
+
+        public int? ParentCategoryId { get; set; }
+        public FoodCategory? ParentCategory { get; set; }
+
+        public ICollection<FoodCategory> SubCategory { get; set; } = new List<FoodCategory>();
+
+        public ICollection<Food>? Food { get; set; }
+>>>>>>> Stashed changes
     }
 }
