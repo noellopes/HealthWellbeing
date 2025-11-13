f1ced048c7ee44aac3e4f240704786dcf4690bb2
ricardorebelo122 <ricardorebelo122@gmail.com>
2025-11-13 17:13:38 +0000

    Alteração do modelo stock e alteracao na view para mostrar o nome do prduto e da zona de armazenamento em vez do id

--- Models/Stock.cs
+++ Models/Stock.cs
@@ -1,13 +1,22 @@
-﻿namespace HealthWellbeing.Models
+﻿using System;
+
+namespace HealthWellbeing.Models
 {
     public class Stock
     {
         public int StockId { get; set; }
+
+        // FK para Consumível e Zona
         public int ConsumivelID { get; set; }
         public int ZonaID { get; set; }
+
+        // Quantidade atual (pode ser futuramente calculada)
         public int QuantidadeAtual { get; set; }
 
+        // Quantidade mínima para alerta
         public int QuantidadeMinima { get; set; }
+
+        // Data da última atualização
         public DateTime DataUltimaAtualizacao { get; set; }
     }
 }
