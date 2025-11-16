cf8df9ae02862522f2e53f5d1790ee88a6c17be8
Leo_Moraes <developerleo.it@gmail.com>
2025-11-16 19:17:27 -0300

    update restricaoAlimentarAlimentos

--- Models/RestricaoAlimentarAlimento.cs
+++ Models/RestricaoAlimentarAlimento.cs
@@ -6,9 +6,9 @@ namespace HealthWellbeing.Models
     public class RestricaoAlimentarAlimento
     {
         public int RestricaoAlimentarId { get; set; }
-        public RestricaoAlimentar RestricaoAlimentar { get; set; }
+        public RestricaoAlimentar? RestricaoAlimentar { get; set; }
 
         public int AlimentoId { get; set; }
-        public Alimento Alimento { get; set; }
+        public Alimento? Alimento { get; set; }
     }
 }
