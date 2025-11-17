393bef734599e145fc70624f13b5d9c447ce87fe
Rafadex42 <rochamiguelbruno@gmail.com>
2025-11-17 11:15:12 +0000

    Add de display no model

--- Models/Pathology.cs
+++ Models/Pathology.cs
@@ -9,12 +9,15 @@ namespace HealthWellbeing.Models
 
         [Required(ErrorMessage = "Name is Required")]
         [StringLength(200, MinimumLength = 3, ErrorMessage = "The name must have at least 3 chars and no more than 100 chars")]
+        [Display(Name = "Nome")]
         public required string Name { get; set; }
 
         [Required(ErrorMessage = "Description is Required")]
+        [Display(Name = "Descrição")]
         public required string Description { get; set; }
 
         [Required(ErrorMessage = "Severity is Required")]
+        [Display(Name = "Gravidade")]
         public required string Severity { get; set; }
 
     }
