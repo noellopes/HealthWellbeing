62146553740c5ec477c769048625b3a77873205f
Mendes444 <goncalo.m.s.mendes@gmail.com>
2025-11-24 10:40:04 +0000

    Alteração do model da zona de armazenamento para que a localização seja FK

--- Models/ZonaArmazenamento.cs
+++ Models/ZonaArmazenamento.cs
@@ -14,9 +14,13 @@ namespace HealthWellbeing.Models
         [StringLength(200, ErrorMessage = "A descrição não pode ter mais de 200 caracteres.")]
         public string? Descricao { get; set; }
 
+
         [Required(ErrorMessage = "A localização é obrigatória.")]
-        [StringLength(150, ErrorMessage = "A localização não pode ter mais de 150 caracteres.")]
-        public string Localizacao { get; set; }
+        [Display(Name = "Localização")]
+        public int LocalizacaoZonaArmazenamentoId { get; set; }
+
+        public LocalizacaoZonaArmazenamento? LocalizacaoZonaArmazenamento { get; set; }
+
 
         [Required(ErrorMessage = "A capacidade máxima é obrigatória.")]
         [Range(1, 10000, ErrorMessage = "A capacidade deve estar entre 1 e 10.000 m³.")]
