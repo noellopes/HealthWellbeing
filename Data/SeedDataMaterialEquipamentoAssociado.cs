2cce507ef0b8f8a97939801518fbbef8a03c97f6
JoaquimGon <joaquimmcgoncalves@gmail.com>
2025-12-04 16:12:31 +0000

    Entidade concluida

--- Data/SeedDataMaterialEquipamentoAssociado.cs
+++ Data/SeedDataMaterialEquipamentoAssociado.cs
@@ -1,11 +1,10 @@
 ﻿// Data/SeedDataProfissionalExecutante.cs (CORRIGIDO)
-using HealthWellbeing.Data;
 using HealthWellbeing.Models;
 using HealthWellBeing.Models;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
 
-namespace HealthWellbeing.Data // <<-- Corrigido o namespace se necessário
+namespace HealthWellbeing.Data
 {
     public class SeedDataMaterialEquipamentoAssociado
     {
@@ -16,10 +15,7 @@ namespace HealthWellbeing.Data // <<-- Corrigido o namespace se necessário
             PopulateEstadoMaterial(dbContext);
             // Chama o método que insere os dados
             PopulateMaterialEquipamentoAssociado(dbContext);
-
-
         }
-
 
         private static void PopulateEstadoMaterial(HealthWellbeingDbContext dbContext)
         {
@@ -40,9 +36,7 @@ namespace HealthWellbeing.Data // <<-- Corrigido o namespace se necessário
             dbContext.SaveChanges();
         }
 
-
-
-        private static void PopulateMaterialEquipamentoAssociado(HealthWellbeingDbContext dbContext) // <<-- Nome do DbContext Corrigido
+        private static void PopulateMaterialEquipamentoAssociado(HealthWellbeingDbContext dbContext)
         {
 
             if (dbContext.MaterialEquipamentoAssociado.Any()) return;
@@ -51,16 +45,12 @@ namespace HealthWellbeing.Data // <<-- Corrigido o namespace se necessário
             // 1. Carrega todos os EstadosMaterial existentes
             var estadosDb = dbContext.EstadosMaterial.ToList();
 
-            // 2. Cria a função de mapeamento NomeEstado -> MaterialStatusId
+            // 2. Mapeamento dos Estados (usando a ENTIDADE completa)
             // NOTA: Usamos 'First' com segurança aqui porque o PopulateEstadoMaterial foi chamado e fez SaveChanges()
-            Func<string, int> getStatusId = nome =>
-                estadosDb.First(e => e.Nome == nome).MaterialStatusId;
-
-            // Mapeamento dos Estados
-
-            int disponivelId = getStatusId("Disponível");
-            int emUsoId = getStatusId("Em Uso");
-            int emManutencaoId = getStatusId("Em Manutenção");
+            // Isso só funciona porque o SaveChanges já ocorreu e os IDs foram gerados.
+            var disponivel = estadosDb.First(e => e.Nome == "Disponível");
+            var emUso = estadosDb.First(e => e.Nome == "Em Uso");
+            var emManutencao = estadosDb.First(e => e.Nome == "Em Manutenção");
 
             var materialEquipamentoAssociado = new[]
             {
@@ -69,182 +59,182 @@ namespace HealthWellbeing.Data // <<-- Corrigido o namespace se necessário
                 {
                     NomeEquipamento = "Seringa Descartável 5ml",
                     Quantidade = 500,
-                    MaterialStatusId = disponivelId,
+                    EstadoMaterial = disponivel
                 },
                 // 2
                 new MaterialEquipamentoAssociado
                 {
                     NomeEquipamento = "Compressa de Gaze Esterilizada",
                     Quantidade = 1200,
-                    MaterialStatusId = disponivelId,
+                    EstadoMaterial = disponivel
                 },
                 // 3
                 new MaterialEquipamentoAssociado
                 {
                     NomeEquipamento = "Monitor de Sinais Vitais",
                     Quantidade = 15,
-                    MaterialStatusId = emUsoId,
+                    EstadoMaterial = emUso
                 },
                 // 4
                 new MaterialEquipamentoAssociado
                 {
                     NomeEquipamento = "Eletrocardiógrafo Portátil",
                     Quantidade = 5,
-                    MaterialStatusId = emManutencaoId,
+                    EstadoMaterial = emManutencao
                 },
                 // 5
                 new MaterialEquipamentoAssociado
                 {
                     NomeEquipamento = "Luvas de Nitrilo (Caixa)",
                     Quantidade = 80,
-                    MaterialStatusId = disponivelId,
+                    EstadoMaterial = disponivel
                 },
                 // 6
                 new MaterialEquipamentoAssociado
                 {
                     NomeEquipamento = "Cadeira de Rodas Standard",
                     Quantidade = 25,
-                    MaterialStatusId = emUsoId,
+                    EstadoMaterial = emUso
                 },
                 // 7
                 new MaterialEquipamentoAssociado
                 {
                     NomeEquipamento = "Bomba de Infusão Volumétrica",
                     Quantidade = 40,
-                    MaterialStatusId = emUsoId,
+                    EstadoMaterial = emUso
                 },
                 // 8
                 new MaterialEquipamentoAssociado
                 {
                     NomeEquipamento = "Termómetro Digital de Testa",
                     Quantidade = 95,
-                    MaterialStatusId = disponivelId,
+                    EstadoMaterial = disponivel
                 },
                 // 9
                 new MaterialEquipamentoAssociado
                 {
                     NomeEquipamento = "Aspirador Cirúrgico",
                     Quantidade = 8,
-                    MaterialStatusId = emUsoId,
+                    EstadoMaterial = emUso
                 },
                 // 10
                 new MaterialEquipamentoAssociado
                 {
                     NomeEquipamento = "Mesa de Cirurgia Multiusos",
                     Quantidade = 3,
-                    MaterialStatusId = emUsoId,
+                    EstadoMaterial = emUso
                 },
                 // 11
                 new MaterialEquipamentoAssociado
                 {
                     NomeEquipamento = "Kit de Sutura Estéril",
                     Quantidade = 300,
-                    MaterialStatusId = disponivelId,
+                    EstadoMaterial = disponivel
                 },
                 // 12
                 new MaterialEquipamentoAssociado
                 {
                     NomeEquipamento = "Bisturi Descartável (Unidade)",
                     Quantidade = 1500,
-                    MaterialStatusId = disponivelId,
+                    EstadoMaterial = disponivel
                 },
                 // 13
                 new MaterialEquipamentoAssociado
                 {
                     NomeEquipamento = "Ventilador Pulmonar",
                     Quantidade = 12,
-                    MaterialStatusId = emManutencaoId,
+                    EstadoMaterial = emManutencao
                 },
                 // 14
                 new MaterialEquipamentoAssociado
                 {
                     NomeEquipamento = "Carro de Emergência (Completo)",
                     Quantidade = 6,
-                    MaterialStatusId = emUsoId,
+                    EstadoMaterial = emUso
                 },
                 // 15
                 new MaterialEquipamentoAssociado
                 {
                     NomeEquipamento = "Agulha Hipodérmica 21G",
                     Quantidade = 2000,
-                    MaterialStatusId = disponivelId,
+                    EstadoMaterial = disponivel
                 },
                 // 16
                 new MaterialEquipamentoAssociado
                 {
                     NomeEquipamento = "Otoscópio/Oftalmoscópio",
                     Quantidade = 18,
-                    MaterialStatusId = emUsoId,
+                    EstadoMaterial = emUso
                 },
                 // 17
                 new MaterialEquipamentoAssociado
                 {
                     NomeEquipamento = "Tala Imobilizadora (Vários Tamanhos)",
                     Quantidade = 75,
-                    MaterialStatusId = disponivelId,
+                    EstadoMaterial = disponivel
                 },
                 // 18
                 new MaterialEquipamentoAssociado
                 {
                     NomeEquipamento = "Esfigmomanómetro Digital",
                     Quantidade = 35,
-                    MaterialStatusId = emUsoId,
+                    EstadoMaterial = disponivel
                 },
                 // 19
                 new MaterialEquipamentoAssociado
                 {
                     NomeEquipamento = "Mascára Cirúrgica N95",
                     Quantidade = 1000,
-                    MaterialStatusId = disponivelId,
+                    EstadoMaterial = disponivel
                 },
                 // 20
                 new MaterialEquipamentoAssociado
                 {
                     NomeEquipamento = "Laringoscópio Completo",
                     Quantidade = 7,
-                    MaterialStatusId = emUsoId,
+                    EstadoMaterial = emUso
                 },
                 // 21
                 new MaterialEquipamentoAssociado
                 {
                     NomeEquipamento = "Fato de Proteção Biológica",
                     Quantidade = 150,
-                    MaterialStatusId = disponivelId,
+                    EstadoMaterial = disponivel
                 },
                 // 22
                 new MaterialEquipamentoAssociado
                 {
                     NomeEquipamento = "Desfibrilhador Externo Automático (DEA)",
                     Quantidade = 10,
-                    MaterialStatusId = emUsoId,
+                    EstadoMaterial = emUso
                 },
                 // 23
                 new MaterialEquipamentoAssociado
                 {
                     NomeEquipamento = "Pilha Alcalina AA (Pack de 10)",
                     Quantidade = 20,
-                    MaterialStatusId = disponivelId,
+                    EstadoMaterial = disponivel
                 },
                 // 24
                 new MaterialEquipamentoAssociado
                 {
                     NomeEquipamento = "Estetoscópio Littmann",
                     Quantidade = 55,
-                    MaterialStatusId = emUsoId,
+                    EstadoMaterial = emUso
                 },
                 // 25
                 new MaterialEquipamentoAssociado
                 {
                     NomeEquipamento = "Balança Hospitalar Digital",
                     Quantidade = 4,
-                    MaterialStatusId = emManutencaoId,
+                    EstadoMaterial = emManutencao
                 },
                 // 26
                 new MaterialEquipamentoAssociado
                 {
                     NomeEquipamento = "Gesso Ortopédico (Rolo)",
                     Quantidade = 90,
-                    MaterialStatusId = disponivelId,
+                    EstadoMaterial = disponivel
                 },
             };
 
@@ -252,4 +242,4 @@ namespace HealthWellbeing.Data // <<-- Corrigido o namespace se necessário
             dbContext.SaveChanges();
         }
     }
-}
+}   
