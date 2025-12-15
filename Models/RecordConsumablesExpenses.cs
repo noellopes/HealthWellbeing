71dc8209f794793a45486f5f7df5e9145165bc66
NunoClodic Carvalho Lima <nunoinformaticaipg@gmail.com>
2025-12-15 18:51:08 +0000

    Adicionado botoes de busca dos materiais nos detalhes da sala Criada a classe registar consumiveis gastos

---
+++ Models/RecordConsumablesExpenses.cs
@@ -0,0 +1,19 @@
+﻿using HealthWellbeing.Models;
+
+namespace HealthWellbeingRoom.Models
+{
+    public class RecordConsumablesExpenses
+    {
+        public int Id { get; set; }
+        public int TreatmentSessionId { get; set; }
+        public int ConsumableId { get; set; }
+        public int Quantity { get; set; }
+        public DateTime Timestamp { get; set; }
+        public int RoomId { get; set; }
+
+        //public TreatmentSession TreatmentSession { get; set; }
+        public Consumivel Consumable { get; set; }
+        public Room Room { get; set; }
+
+    }
+}
