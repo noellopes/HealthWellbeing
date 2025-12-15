6b17b505d302e93c732d2973746cbde37f05112c
DiogoGH <diogorsilva04@gmail.com>
2025-12-15 17:22:46 +0000

    Tabela tipo de eventos e a sua ligaçao

---
+++ Models/ActivityType.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HealthWellbeing.Models
+{
+    public class ActivityType
+    {
+        public int ActivityTypeId { get; set; }
+
+        [Required]
+        public string ActivityTypeName { get; set; } = string.Empty;
+
+
+        [Required]
+        public string ActivityDescription { get; set; } = string.Empty;
+
+        public virtual ICollection <Activity>? Activity { get; set; }
+
+    }
+}
