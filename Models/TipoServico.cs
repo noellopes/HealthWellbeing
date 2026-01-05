822fa0ec60892c453e153b8e6ae4ba9efb5803d4
dasilva-2024 <arturfilipe24@gmail.com>
2026-01-05 17:46:58 +0000

    Atualizando o modelo tipo servico

--- Models/TipoServico.cs
+++ Models/TipoServico.cs
@@ -9,12 +9,12 @@ namespace HealthWellbeing.Models
         public int TipoServicosId { get; set; }
 
         [Required(ErrorMessage = "O nome do tipo de serviço é obrigatório.")]
-        [RegularExpression(@"^[a-zA-ZÀ-ÿ\s]+$", ErrorMessage = "O nome não pode conter números ou caracteres especiais.")]
+        [RegularExpression(@"^[^0-9]+$", ErrorMessage = "O nome não pode conter números.")]
         [StringLength(100, ErrorMessage = "O nome é demasiado longo.")]
         public string Nome { get; set; }// Ex.: "massagens", "banhos", "tratamentos", "fisioterapia", "Programas de bem-estar"
 
         [Display(Name = "Descrição")]
-        [RegularExpression(@"^[a-zA-ZÀ-ÿ\s]+$", ErrorMessage = "A descrição não pode conter números ou caracteres especiais.")]
+        [RegularExpression(@"^[^0-9]+$", ErrorMessage = "A descrição não pode conter números.")]
         public string? Descricao { get; set; }// Detalhes do tipo de serviço
 
         
