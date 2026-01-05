31196bff3a47739096303c240f5c8974ee1f35b2
Diogo Cardoso <1709197@sal.ipg.pt>
2026-01-05 17:16:33 +0000

    Rollback in file

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,11 +1,16 @@
 ﻿using HealthWellbeing.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 namespace HealthWellbeing.Data
 {
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser, IdentityRole, string>
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        {
+        }
     }
 }
 
