c333df76f375f1ea5f7c6300da11aa29fda962de
Diogo Cardoso <1709197@sal.ipg.pt>
2026-01-05 17:09:28 +0000

    Cleaned project

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using HealthWellbeing.Models;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HealthWellbeing.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public int? ClientId { get; set; }
+
+        [ForeignKey(nameof(ClientId))]
+        public Client? Client { get; set; }
+    }
+}
