dfbb21f1ce0f4070e50ac48fe139a72c8b63ac0d
Rafael Ricardo <rafael.reis.ricardo@live.com.pt>
2026-01-06 21:33:29 +0000

    Fluent API adicionado

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -11,6 +11,7 @@ namespace HealthWellbeing.Data
         {
         }
 
+        //DBSets
         public DbSet<UtenteBalneario> Utentes { get; set; }
         public DbSet<DadosMedicos> DadosMedicos { get; set; }
         public DbSet<SeguroSaude> SegurosSaude { get; set; }
@@ -20,6 +21,41 @@ namespace HealthWellbeing.Data
         public DbSet<AgendamentoModel> Agendamentos { get; set; }
         public DbSet<TipoServico> TipoServicos{ get; set; }
 
-       
+
+        //Fluent API
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // RELAÇÃO 1–1 Utente → DadosMedicos
+            builder.Entity<UtenteBalneario>()
+                .HasOne(u => u.DadosMedicos)
+                .WithOne()
+                .HasForeignKey<UtenteBalneario>(u => u.DadosMedicos)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            // RELAÇÃO 1–1 Utente → SeguroSaude
+            builder.Entity<UtenteBalneario>()
+                .HasOne(u => u.SeguroSaude)
+                .WithOne()
+                .HasForeignKey<UtenteBalneario>(u => u.SeguroSaude)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            // Regras adicionais (Fluent API)
+            builder.Entity<UtenteBalneario>()
+                .Property(u => u.NomeCompleto)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Entity<UtenteBalneario>()
+                .Property(u => u.Morada)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            builder.Entity<UtenteBalneario>()
+                .Property(u => u.NIF)
+                .IsRequired()
+                .HasMaxLength(9);
+        }
     }
 }
