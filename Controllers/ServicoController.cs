a4a4db414f1b54bf62da49c8ecf151120e01f4ad
dasilva-2024 <arturfilipe24@gmail.com>
2026-01-07 07:27:52 +0000

    Index de Serviço alinhado com Tipo de Serviço (pesquisa e paginação)

--- Controllers/ServicoController.cs
+++ Controllers/ServicoController.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using HealthWellbeing.Data;
-using HealthWellbeing.Models;
 
 namespace HealthWellbeing.Controllers
 {
