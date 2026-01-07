40f8d3b3c4f9fa292a4bcc0cb914178b5490cc61
dasilva-2024 <arturfilipe24@gmail.com>
2026-01-07 08:22:12 +0000

    Corrigindo o sistema de numero de paginas

--- Models/Paginacao.cs
+++ Models/Paginacao.cs
@@ -1,24 +1,25 @@
 ﻿namespace HealthWellbeing.Models
 {
-    
-        public class Paginacao
-        {
-        private int totalRegistos;
-        private int pagina;
-        private int pageSize;
+    public class Paginacao
+    {
+        public int PaginaCorrente { get; private set; }
+        public int ItemTotal { get; private set; }
+        public int TamanhoPagina { get; private set; }
+
+        public int PaginaTotal =>
+            (int)Math.Ceiling((double)ItemTotal / TamanhoPagina);
+
+        public int PrimeiraPaginaVer =>
+            Math.Max(1, PaginaCorrente - 2);
+
+        public int UltimaPaginaVer =>
+            Math.Min(PaginaTotal, PaginaCorrente + 2);
 
         public Paginacao(int totalRegistos, int pagina, int pageSize)
         {
-            this.totalRegistos = totalRegistos;
-            this.pagina = pagina;
-            this.pageSize = pageSize;
-        }
-
-        public int PaginaCorrente { get; set; }
-            public int ItemTotal { get; set; }
-            public int TamanhoPagina { get; set; } = 6;
-            public int PaginaTotal => (int)Math.Ceiling((decimal)ItemTotal / TamanhoPagina);
-            public int PrimeiraPaginaVer => Math.Max(1, PaginaCorrente - 5);
-            public int UltimaPaginaVer => Math.Min(PaginaTotal, PaginaCorrente + 5);
+            ItemTotal = totalRegistos;
+            PaginaCorrente = pagina < 1 ? 1 : pagina;
+            TamanhoPagina = pageSize;
         }
     }
+}
