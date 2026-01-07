f79f98c875c5f05c77bc9efee1b37f363700c596
Diogo Cardoso <1709197@sal.ipg.pt>
2026-01-07 01:29:08 +0000

    Added back Plan

---
+++ Models/Plan.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HealthWellbeing.Models
+{
+    public class Plan
+    {
+        public int PlanId { get; set; }
+
+        [Required(ErrorMessage = "The plan name is required")]
+        [StringLength(100, ErrorMessage = "The plan name cannot exceed 100 characters")]
+        public string Name { get; set; } = default!;
+
+        public string? Description { get; set; }
+
+        [Required(ErrorMessage = "The plan price is required")]
+        [Range(1.00, 999.99, ErrorMessage = "The price must be between 1.00 and 999.99")]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "The training duration is required")]
+        [Range(1, 365, ErrorMessage = "The duration must be between 1 and 365 days")]
+        public int DurationDays { get; set; }
+    }
+}
