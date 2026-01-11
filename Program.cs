1c051ea4708e894fef222f2125b66a5f15cbb05c
dasilva-2024 <arturfilipe24@gmail.com>
2026-01-11 14:06:20 +0000

    Removendo a duplicação do registro de ApplicationDbContext

--- Program.cs
+++ Program.cs
@@ -8,19 +8,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<HealthWellbeingDbContext>(options =>
 	options.UseSqlServer(builder.Configuration.GetConnectionString("HealthWellbeingConnection") ?? throw new InvalidOperationException("Connection string 'HealthWellbeingConnection' not found.")));
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
-builder.Services.AddDbContext<HealthWellbeingDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
-
 
 
 var app = builder.Build();
