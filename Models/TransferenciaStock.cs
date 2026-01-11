9f28b33ddeef3cd9f7f7079c77afe8991d7a91cf
Mendes444 <goncalo.m.s.mendes@gmail.com>
2026-01-11 15:01:50 +0000

    Criação do model TransferirStock, da view Tranferir e atualizações necessárias no controlador e index para fazer a tranferência de stock entre zonas de armazenameto

---
+++ Models/TransferenciaStock.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HealthWellbeing.Models 
+{
+    
+    public class TransferenciaStock
+    {
+        public int ZonaOrigemId { get; set; }
+        public string? ZonaOrigemNome { get; set; }
+        public string? ConsumivelNome { get; set; }
+        public int QuantidadeAtualOrigem { get; set; }
+
+        [Required(ErrorMessage = "Selecione a zona de destino.")]
+        [Display(Name = "Zona de Destino")]
+        public int ZonaDestinoId { get; set; }
+
+        [Required(ErrorMessage = "Insira a quantidade a transferir.")]
+        [Range(1, int.MaxValue, ErrorMessage = "A quantidade deve ser superior a 0.")]
+        [Display(Name = "Quantidade a Transferir")]
+        public int QuantidadeATransferir { get; set; }
+    }
+}
