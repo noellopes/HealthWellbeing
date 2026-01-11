0200914dbe67ba79913e93271d51b60d903c50c9
Bruno <Bruno@LAPTOP-BRUNO>
2026-01-11 18:01:21 +0000

    criação dos viewModels Client e Trainer

---
+++ ViewModels/ClientInputModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace HealthWellbeing.ViewModels
+{
+    public class ClientInputModel
+    {
+        [Required(ErrorMessage = "Please enter your name.")]
+        [StringLength(100, MinimumLength = 6)]
+        public required string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email.")]
+        [EmailAddress]
+        public required string Email { get; set; }
+
+        [Required(ErrorMessage = "A phone number is required.")]
+        [Phone]
+        public required string PhoneNumber { get; set; }
+
+        [DataType(DataType.Date)]
+        [Required(ErrorMessage = "Birth date is mandatory.")]
+        public DateTime BirthDate { get; set; }
+
+        [Required(ErrorMessage = "Please enter a password.")]
+        [PasswordPropertyText]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "The {0} must be at least {2} characters long.")]
+        public required string Password { get; set; }
+
+        [Required(ErrorMessage = "Please confirm your password.")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [Display(Name = "Confirm password")]
+        [PasswordPropertyText]
+        public required string ConfirmPassword { get; set; }
+    }
+}
---
+++ ViewModels/TrainerInputModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HealthWellbeing.ViewModels
+{
+    public class TrainerInputModel
+    {
+        public int? TrainerId { get; set; }
+
+        [Required(ErrorMessage = "Please enter the trainer's name.")]
+        [StringLength(100)]
+        public required string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter the work email.")]
+        [EmailAddress]
+        public required string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter the contact phone.")]
+        [Phone]
+        public required string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Specify the professional specialty.")]
+        public string Specialty { get; set; } = string.Empty;
+
+        public bool IsAdministrator { get; set; } = false;
+        public bool IsTrainer { get; set; } = true;
+    }
+}
