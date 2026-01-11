fe298a3bf447fcc0e06a86e7b61a381305b17c68
dasilva-2024 <arturfilipe24@gmail.com>
2026-01-11 07:41:14 +0000

    Correção das migrações e sincronização do EF Core com a base de dados existente

--- Controllers/AgendamentoBalneariosController.cs
+++ Controllers/AgendamentoBalneariosController.cs
@@ -95,8 +95,6 @@ namespace HealthWellbeing.Controllers
         }
 
         // POST: AgendamentoBalnearios/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("AgendamentoId,UtenteBalnearioId,TerapeutaId,ServicoId,TipoServicosId,HoraInicio,DuracaoMinutos,Preco,Descricao,Estado")] AgendamentoBalneario agendamentoBalneario)
