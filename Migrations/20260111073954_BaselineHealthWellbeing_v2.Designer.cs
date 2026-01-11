c833b952ecaf95ac9b4f4bdb6e48fb906962ae01
dasilva-2024 <arturfilipe24@gmail.com>
2026-01-11 07:43:33 +0000

    Criação da migração e correção da view de criação de agendamentos

--- Migrations/20260111073954_BaselineHealthWellbeing_v2.Designer.cs
+++ Migrations/20260111073954_BaselineHealthWellbeing_v2.Designer.cs
@@ -4,6 +4,7 @@ using HealthWellbeing.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HealthWellbeing.Migrations
 {
     [DbContext(typeof(HealthWellbeingDbContext))]
-    partial class HealthWellbeingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260111073954_BaselineHealthWellbeing_v2")]
+    partial class BaselineHealthWellbeing_v2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
