8eedd050dc4f9f36adb7d0c6ad70adb421c30e61
Rafael Ricardo <rafael.reis.ricardo@live.com.pt>
2026-01-27 18:14:06 +0000

    Add UtenteBalneario controller com Index e Details

---
+++ Controllers/UtenteBalnearioController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HealthWellbeing.Data;
+using HealthWellbeing.Models;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace HealthWellbeing.Controllers
+{
+    public class UtenteBalnearioController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UtenteBalnearioController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: UtenteBalneario
+        public async Task<IActionResult> Index()
+        {
+            var utentes = await _context.UtenteBalnearios.ToListAsync();
+            return View(utentes);
+        }
+
+        // GET: UtenteBalneario/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var utente = await _context.UtenteBalnearios
+                .FirstOrDefaultAsync(u => u.UtenteBalnearioId == id);
+
+            if (utente == null)
+                return NotFound();
+
+            return View(utente);
+        }
+    }
+}
